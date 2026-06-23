b708034f1c2c8d6e2ee0c541ce9cbaf0d9c15948
Anjali <shishodiaanjali597@gmail.com>
2026-06-23 11:36:50 +0530

    updated oneD array codes

---
+++ oneDarray.java
@@ -0,0 +1,29 @@
+//public class oneDarray {
+    //public static void main(String[]args){
+       // int arr[]={10,20,30,40};
+        //TO PRINT MULTIPLICATION OF 1-D ARRAY
+        /*int sum=0;
+        int n =arr.length;
+        for(int i=0;i<=n-1;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("Sum of array is :"+sum);*/
+        //TO PRINT MULTIPLICATION OF 1-D ARRAY
+        /*int ans=1;
+        int n=arr.length;
+        for(int i=0;i<=n-1;i++){
+            ans=ans*arr[i];
+        }
+        System.out.println("the multiplication of array is =  "+ans);*/
+        //TO PRINT MAXIMUM ELEMEMENT IN 1-D ARRAY
+        /*int maxvalue=arr[0];
+        int n=arr.length;
+        for(int i=0;i<=n-1;i++){
+            if(arr[i] > maxvalue){
+            maxvalue=arr[i];}
+        }
+        System.out.println("The maximum value of array is = "+maxvalue);*/
+
+    //}   
+    
+//}
